b8176405554f480b5ac0eeba4f4f268e655cb1d2
Priyanshi Gupta <priyanshigupta997@gmail.com>
2021-01-18 22:50:15 +0530

    Required Routes registered

--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
-// /admin/add-product => GET
-router.get('/add-product', productsController.getAddProduct);
+// /admin => GET
+router.get('/add-product', adminController.getAddProduct);
+router.get('/products', adminController.getProducts);
 
-// /admin/add-product => POST
-router.post('/add-product', productsController.postAddProduct);
+// /admin => POST
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
