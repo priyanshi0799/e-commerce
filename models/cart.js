6c2c17e7685d40f18cd2457e885e723d704430f4
Priyanshi Gupta <priyanshigupta997@gmail.com>
2021-01-23 18:09:39 +0530

    Cart view added

--- models/cart.js
+++ models/cart.js
@@ -68,4 +68,15 @@ module.exports = class Cart {
             }
         });
     }
+
+    static getProducts(callback) {
+        fs.readFile(p, (err, fileContent) => {
+            const cart = JSON.parse(fileContent);
+            if (err) {
+                callback(null);
+            } else {
+                callback(cart);
+            }
+        });
+    }
 };
