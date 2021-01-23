bce15817ec07ddadd996599c912a9daaa53ab5dc
Priyanshi Gupta <priyanshigupta997@gmail.com>
2021-01-23 12:50:20 +0530

    Minor changes

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,6 +4,7 @@ exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
+        editing: false,
     });
 };
 
@@ -22,14 +23,21 @@ exports.getEditProduct = (req, res, next) => {
     const editMode = req.query.edit; //always a string
 
     if (editMode !== true) {
-        res.redirect('/');
-    } else {
+        return res.redirect('/');
+    }
+    const prodId = req.params.productId;
+
+    Product.findById(prodId, (product) => {
+        if (!product) {
+            return res.redirect('/');
+        }
         res.render('admin/edit-product', {
             pageTitle: 'Edit Product',
             path: '/admin/edit-product',
             editing: editMode,
+            product: product,
         });
-    }
+    });
 };
 
 exports.getProducts = (req, res, next) => {
